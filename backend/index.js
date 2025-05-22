b987a092a46957ead51036efa44b08a8371a2eff
vkveka <bernardvicari@gmail.com>
2025-05-22 14:04:08 +0200

    backend init, mongo not working

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
+ 
